96414b22a175c669448d5d3f58227e9d958a729b
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-04-09 01:37:26 +0200

    added ScoreValidator and Impl

---
+++ src/main/kotlin/pl/kisielovepiatki/backend/validator/impl/ScoreValidator.kt
@@ -0,0 +1,7 @@
+package pl.kisielovepiatki.backend.validator.impl
+
+import pl.kisielovepiatki.backend.model.entity.survey.Score
+import pl.kisielovepiatki.backend.validator.GenericValidator
+
+interface ScoreValidator: GenericValidator<Score> {
+}
