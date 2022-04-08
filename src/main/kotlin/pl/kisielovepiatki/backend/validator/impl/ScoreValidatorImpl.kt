63a09c3a6577e634675d87426c507f5fc29f4635
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-04-09 01:39:51 +0200

    added ScoreValidatorImpl

---
+++ src/main/kotlin/pl/kisielovepiatki/backend/validator/impl/ScoreValidatorImpl.kt
@@ -0,0 +1,36 @@
+package pl.kisielovepiatki.backend.validator.impl
+
+import org.springframework.stereotype.Component
+import pl.kisielovepiatki.backend.model.entity.survey.Score
+import pl.kisielovepiatki.backend.validator.ValidationResult
+
+@Component
+class ScoreValidatorImpl : ScoreValidator, GenericValidatorImpl<Score>() {
+
+    companion object {
+        const val BLANK_NAME_NOT_ALLOWED_MESSAGE: String = "Name can't be blank"
+        const val EMPTY_NAME_NOT_ALLOWED_MESSAGE: String = "Name can't be empty"
+        const val NAME_NOT_MATCHING_REGEX: String = "Name can contain only letters and spaces"
+    }
+
+    override fun isValid(obj: Score): ValidationResult {
+        return isNameValid(obj.name)
+    }
+
+    private fun isNameValid(name: String): ValidationResult {
+        if(name.isBlank()) {
+            return ValidationResult(false, BLANK_NAME_NOT_ALLOWED_MESSAGE)
+        }
+
+        if(name.isEmpty()) {
+            return ValidationResult(false, EMPTY_NAME_NOT_ALLOWED_MESSAGE)
+        }
+
+        if(!Regex("([A-Z a-z])+").matches(name)) {
+            return ValidationResult(false, NAME_NOT_MATCHING_REGEX)
+        }
+
+        return ValidationResult(true, null)
+    }
+
+}
