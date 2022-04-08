d62ef10fcfd6aadbd5904ca0decbc72afc65a900
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-04-09 01:46:51 +0200

    passing ScoreValidator to ScoreServiceImpl, deleted AnswerServiceImpl

--- src/main/kotlin/pl/kisielovepiatki/backend/service/domain/impl/ScoreServiceImpl.kt
+++ src/main/kotlin/pl/kisielovepiatki/backend/service/domain/impl/ScoreServiceImpl.kt
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Service
 import pl.kisielovepiatki.backend.model.entity.survey.Score
 import pl.kisielovepiatki.backend.repository.ScoreRepository
 import pl.kisielovepiatki.backend.service.domain.ScoreService
+import pl.kisielovepiatki.backend.validator.impl.ScoreValidator
 
 @Service
 class ScoreServiceImpl(
-        private val scoreRepository: ScoreRepository
+    private val scoreRepository: ScoreRepository,
+    private val scoreValidator: ScoreValidator
 ) : ScoreService, GenericEntityServiceImpl<Score, Int>(
-        scoreRepository
+    scoreRepository,
+    scoreValidator
 )
